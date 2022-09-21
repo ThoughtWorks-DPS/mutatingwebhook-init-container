215c9e9cf55d5724823bbaf2219e006b34d5631e
Bryan Oliver <boliver@linux.com>
2022-09-21 11:38:49 -0400

    [olivercodes,ncheneweth] - mutating webhook configurations list method

--- main.go
+++ main.go
@@ -8,14 +8,17 @@ import (
 	// "crypto/x509/pkix"
 	// "encoding/pem"
 	// "fmt"
+
 	"log"
+
 	// "strings"
 	// "path"
 	// "math/big"
 	"os"
 	// "time"
-	"flag"
 	"context"
+	"flag"
+
 	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -23,20 +26,20 @@ import (
 )
 
 var (
-	webhookName					string
-	objectMetaName				string
-	serviceName					string
-	serviceNamespace			string
-	servicePath					string
-	caBundle					string
-	namespaceSelectorKey		string
-	namespaceSelectorValue  	string
-	certPath					string
+	webhookName            string
+	objectMetaName         string
+	serviceName            string
+	serviceNamespace       string
+	servicePath            string
+	caBundle               string
+	namespaceSelectorKey   string
+	namespaceSelectorValue string
+	certPath               string
 )
 
 func main() {
 	// var certificatePEM *bytes.Buffer
-	reviewVersion := []string {"v1beta1"}
+	reviewVersion := []string{"v1beta1"}
 	flag.StringVar(&webhookName, "webhook-name", "", "Webhook name for MutatingWebhookConfiguration, required")
 	flag.StringVar(&objectMetaName, "object-meta-name", "sidecarinjector.twdps.io", "ObjectMeta name for MutatingWebhookConfiguration, default is sidecarinjector.twdps.io")
 	flag.StringVar(&serviceName, "service-name", "", "ClientConfig service name, required")
@@ -57,7 +60,7 @@ func main() {
 	log.Printf("ca-bundle: %s", caBundle)
 	log.Printf("namespace-selector-key: %s", namespaceSelectorKey)
 	log.Printf("namespace-selector-value: %s", namespaceSelectorValue)
-	log.Printf("cert-path: %s",certPath)
+	log.Printf("cert-path: %s", certPath)
 
 	// read CA Bundle
 	certificatePEM, err := os.ReadFile(certPath)
@@ -70,7 +73,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to set go -client: %s", err)
 	}
-	
+
 	// create mutatingwebhookconfiguration resource request
 	fail := admissionregistrationv1.Fail
 	none := admissionregistrationv1.SideEffectClassNone
@@ -101,19 +104,22 @@ func main() {
 					namespaceSelectorKey: namespaceSelectorValue,
 				},
 			},
-			FailurePolicy: &fail,
+			FailurePolicy:           &fail,
 			AdmissionReviewVersions: reviewVersion,
-			SideEffects: &none,
+			SideEffects:             &none,
 		}},
 	}
 
-	// if theList, err := kubeClient.AdmissionregistrationV1().MutatingWebhookConfigurationsList()
-	// 	err != nil {
+	listM, err := kubeClient.AdmissionregistrationV1().MutatingWebhookConfigurations().List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		log.Fatalf("err listing mutating configs - %v", err)
+	}
+
+	log.Printf("%s", listM)
+
+	// if _, err := kubeClient.AdmissionregistrationV1().MutatingWebhookConfigurations().Create(context.Background(), mutateconfig, metav1.CreateOptions{}); err != nil {
 	// 	panic(err)
 	// }
-
-	if _, err := kubeClient.AdmissionregistrationV1().MutatingWebhookConfigurations().Create(context.Background(), mutateconfig, metav1.CreateOptions{})
-		err != nil {
-		panic(err)
-	}
+	// TODO - Remove when rdy to create
+	log.Printf("%s", mutateconfig)
 }
