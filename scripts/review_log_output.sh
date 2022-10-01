807d9fe640f8f338c81e6cd0b1e2f223e4645ecd
Nic Cheneweth <nchenewe@thoughtworks.com>
2022-09-30 21:48:29 -0500

    [nc] adjust smoke tests
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/review_log_output.sh
+++ scripts/review_log_output.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker pull twdps/sidecar-mutatingwebhook-init-container
-export RESULT=$(docker run -it twdps/sidecar-mutatingwebhook-init-container:latest | grep "Success: created mutatingwebhookconfiguration")
+export RESULT=$(docker run -it twdps/sidecar-mutatingwebhook-init-container:latest  | grep "Requested MutatingWebhookConfiguration")
 if [[ "${RESULT}" == "" ]]; then
   echo 'Container did not log success'
   exit 1
